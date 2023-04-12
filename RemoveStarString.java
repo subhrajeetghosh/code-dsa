b466d51f726c4c186e547a9e9082deb2a87ac287
Subhrajeet Ghosh <subhrajeet.ghosh@oracle.com>
2023-04-12 09:47:52 +0530

    added removestar and valid parentheses

---
+++ RemoveStarString.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+public class RemoveStarString {
+    public String removeStars(String s) {
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != '*') {
+                st.add(s.charAt(i));
+            } else {
+                st.pop();
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!st.isEmpty()) {
+            sb.append(st.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
---
+++ ValidParentheses.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                st.add(s.charAt(i));
+            } else {
+                if (!st.isEmpty()) {
+                    if (s.charAt(i) == ')' && st.peek() == '(' || s.charAt(i) == '}' && st.peek() == '{'
+                            || s.charAt(i) == ']' && st.peek() == '[') {
+                        st.pop();
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
