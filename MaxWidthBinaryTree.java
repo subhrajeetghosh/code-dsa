747cfc5989a26af972890be930bfcf7d3245e519
subhrajeet <subhrajeet.ankhi@gmail.com>
2023-04-23 21:49:01 +0530

    modified maxWidthBinTree Program

--- MaxWidthBinaryTree.java
+++ MaxWidthBinaryTree.java
@@ -22,20 +22,40 @@ public class MaxWidthBinaryTree {
                 Math.max(dfs(node.left, index * 2, depth + 1), dfs(node.right, index * 2 + 1, depth + 1)));
     }
 
-    //Implement in BFS
+    // Implement in BFS
     public int widthOfBinaryTreeBfs(TreeNode root) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        Queue<TreeNode> node = new LinkedList<>();
-        node.add(root);
-        int depth = 1;
-        int index = 1;
-        int diff = 0;
-        while(!node.isEmpty()) {
-            TreeNode currentNode = node.poll();
-            if(currentNode.left != null) {
-                
-            }
+        if(root==null) {
+            return 0;
         }
-        return 0;
+        Queue<TreeNode> node = new LinkedList<>();
+        Queue<Integer> index = new LinkedList<>();
+        int maxWidth = 1;
+        node.add(root);
+        index.add(1);
+        while (!node.isEmpty()) {
+            int queueLength = node.size();
+            int leftIndex = 0; 
+            int rightIndex = 0;
+            for (int i = 0; i < queueLength; i++) {
+                TreeNode currentNode = node.poll();
+                int currentIndex = index.poll();
+                if (currentNode.left != null) {
+                    node.add(currentNode.left);
+                    index.add(currentIndex * 2);
+                }
+                if (currentNode.right != null) {
+                    node.add(currentNode.right);
+                    index.add(currentIndex * 2 + 1);
+                }
+                if (i == 0) {
+                    leftIndex = currentIndex;
+                }
+                if (i == queueLength - 1) {
+                    rightIndex = currentIndex;
+                }
+            }
+            maxWidth = Math.max(maxWidth, rightIndex-leftIndex+1);
+        }
+        return maxWidth;
     }
 }
