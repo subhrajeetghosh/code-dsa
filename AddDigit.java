348bf5e64dec160b4b9e60983a2a5b3f98d0ccd5
Subhrajeet Ghosh <subhrajeet.ghosh@oracle.com>
2023-04-27 00:51:41 +0530

    Add Digit

---
+++ AddDigit.java
@@ -0,0 +1,13 @@
+public class AddDigit {
+    public int addDigits(int num) {
+        while(num/10 != 0) {
+            int dummyNum = 0;
+            while(num > 0) {
+                dummyNum += num%10;
+                num /= 10;
+            }
+            num = dummyNum;
+        }
+        return num;
+    }
+}
--- DecodeWays.java
+++ DecodeWays.java
@@ -1,10 +1,11 @@
 public class DecodeWays {
 
-    //Bruteforce way with recursion
+    // Bruteforce way with recursion
     int count = 0;
 
     public int decodeStringWays(String s) {
-        recForCount(s, 0);
+        // recForCount(s, 0);
+        recForCountSecondMethod(s, 0);
         return count;
     }
 
@@ -24,15 +25,47 @@ public class DecodeWays {
         }
     }
 
-    //Memorization
+    // Second recursive method
 
+    public void recForCountSecondMethod(String s, int start) {
+        if (s.length() <= start) {
+            count++;
+            return;
+        }
+        int num = s.charAt(start) - '0';
+        if (num == 0) {
+            return;
+        }
+        boolean isSecondNumValid = false;
+        if (start <= s.length() - 2) {
+            int secondNum = Integer.parseInt(s.substring(start, start + 2));
+            if (secondNum < 27 && secondNum >= 10) {
+                isSecondNumValid = true;
+                recForCountSecondMethod(s, start + 1);
+                recForCountSecondMethod(s, start + 2);
+            }
+        }
+        if (!isSecondNumValid) {
+            recForCountSecondMethod(s, start + 1);
+        }
+    }
+
+    // Memorization
+
+    public int memorizationOnRecursionForCount(String s, int start, int[] arr) {
+        if (s.length() >= start) {
+            return 0;
+        }
+        if (arr[start] != -1)
+            return arr[start];
+        return arr[start];
+    }
 
     public static void main(String[] args) {
         DecodeWays obj = new DecodeWays();
         System.out.println(obj.decodeStringWays("121"));
     }
 }
-
 
 /*
  * Dynamic Problem
