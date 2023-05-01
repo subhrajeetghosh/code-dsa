a6b1a3404fbcc60dba5c0578d40c3418000820a1
Subhrajeet Ghosh <subhrajeet.ghosh@oracle.com>
2023-05-01 13:16:19 +0530

    added format

--- AverageSalaryExcludingMinMax.java
+++ AverageSalaryExcludingMinMax.java
@@ -1,20 +1,20 @@
 /**
  * https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary
+ * 
  * @author Subhrajeet Ghosh
  */
 
- 
 public class AverageSalaryExcludingMinMax {
     public double average(int[] salary) {
         double total = 0;
         int minSalary = Integer.MIN_VALUE;
         int maxSalary = Integer.MAX_VALUE;
-        for(int i=0; i<salary.length; i++) {
+        for (int i = 0; i < salary.length; i++) {
             minSalary = Math.min(minSalary, salary[i]);
             maxSalary = Math.max(maxSalary, salary[i]);
             total += salary[i];
         }
         total -= (minSalary + maxSalary);
-        return total/(salary.length-2);
+        return total / (salary.length - 2);
     }
 }
