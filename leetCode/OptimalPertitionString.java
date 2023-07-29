b1cdb47413bdaf7f2edaa30172eda55fbfc11f8d
Subhrajeet Ghosh <subhrajeet.ghosh@oracle.com>
2023-07-30 01:09:09 +0530

    part 4

---
+++ leetCode/OptimalPertitionString.java
@@ -0,0 +1,33 @@
+package leetCode;
+import java.util.HashSet;
+import java.util.Set;
+
+public class OptimalPertitionString {
+    public int partitionString(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            Set<Character> set = new HashSet<>();
+            count++;
+            for (int j = i; j < s.length(); j++) {
+                if (!set.add(s.charAt(j))) {
+                    i = j - 1;
+                    break;
+                } else if (j == s.length() - 1) {
+                    i = j;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        String str = new String("cuieokbs");
+        OptimalPertitionString obj = new OptimalPertitionString();
+        System.out.println(obj.partitionString(str));
+    }
+}
+
+
+/**
+ * https://leetcode.com/problems/optimal-partition-of-string/
+ */
