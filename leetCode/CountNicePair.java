7a3c52adefc1c1447b7b31c67e33f536cdc45ca9
Subhrajeet Ghosh <subhrajeet.ghosh@oracle.com>
2023-11-23 12:15:56 +0530

    lc

--- leetCode/CountNicePair.java
+++ leetCode/CountNicePair.java
@@ -1,5 +1,8 @@
 package leetCode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/count-nice-pairs-in-an-array
  * 
@@ -8,19 +11,34 @@ package leetCode;
 
 public class CountNicePair {
 
-    //BruteForce Approch
+    // BruteForce Approch
     public int countNicePairs(int[] nums) {
         int[] revVal = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
             revVal[i] = getRevVal(nums[i]);
         }
         int count = 0;
-        for(int i=0; i<nums.length; i++) {
-            for(int j=i+1; j<nums.length; j++) {
-                if(nums[i]+revVal[j] == nums[j]+revVal[i]) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + revVal[j] == nums[j] + revVal[i]) {
                     count++;
                 }
             }
+        }
+        return count;
+    }
+
+    // Optimized Approch
+    public int countNicePairs_2ndMethod(int[] nums) {
+        int[] revVal = new int[nums.length];
+        int count = 0;
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            revVal[i] = getRevVal(nums[i]);
+            int difference = nums[i] - revVal[i];
+            count += freqMap.getOrDefault(difference, 0);
+            count %= 1_000_000_007;
+            freqMap.put(difference, freqMap.getOrDefault(difference, 0) + 1);
         }
         return count;
     }
