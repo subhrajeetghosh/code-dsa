29b5015f34b9b6b3c357660a753714cedd9a1add
Subhrajeet Ghosh <subhrajeet.ghosh@oracle.com>
2023-12-20 17:37:55 +0530

    20dec23

---
+++ leetCode/TwoChoco.java
@@ -0,0 +1,27 @@
+package leetCode;
+
+import java.util.Arrays;
+
+public class TwoChoco {
+    public int buyChoco_2ndMethod(int[] prices, int money) { // time complexity - O(n log n)
+        Arrays.sort(prices);
+        int res = money - prices[0] - prices[1];
+        return res >= 0 ? res : money;
+    }
+
+    public int buyChoco(int[] prices, int money) { // time complexity - O(n)
+        int lowest = Integer.MAX_VALUE;
+        int nextLowest = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            if (lowest >= prices[i]) {
+                nextLowest = lowest;
+                lowest = prices[i];
+            } else if (nextLowest > prices[i]) {
+                nextLowest = prices[i];
+            }
+        }
+        System.out.println(lowest + " " + nextLowest);
+        int res = money - lowest - nextLowest;
+        return res >= 0 ? res : money;
+    }
+}
