b94c0607b6ce5f709b0e7d3396cc81bee0f942b0
Subhrajeet Ghosh <subhrajeet.ghosh@oracle.com>
2023-12-27 19:32:39 +0530

    27dec23

---
+++ leetCode/MinRopeColour.java
@@ -0,0 +1,21 @@
+package leetCode;
+
+/**
+ * https://leetcode.com/problems/minimum-time-to-make-rope-colorful
+ * 
+ * @author Subhrajeet Ghosh
+ */
+
+public class MinRopeColour {
+    public int minCost(String colors, int[] neededTime) { // time complexity - O(n)
+        int ans = 0;
+        char[] charArray = colors.toCharArray();
+        for (int i = 0; i < neededTime.length - 1; i++) {
+            if (charArray[i] == charArray[i + 1]) {
+                ans += Math.min(neededTime[i], neededTime[i + 1]);
+                neededTime[i + 1] = Math.max(neededTime[i], neededTime[i + 1]);
+            }
+        }
+        return ans;
+    }
+}
