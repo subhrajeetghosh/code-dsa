79bc583409aa06fd275675bfa6b25b3ca2364d81
Subhrajeet Ghosh <subhrajeet.ghosh@oracle.com>
2024-02-01 18:59:55 +0530

    01feb24

---
+++ leetCode/DivideArrayToArrays.java
@@ -0,0 +1,22 @@
+package leetCode;
+
+import java.util.Arrays;
+
+/**
+ * @author subhrajeetghosh
+ */
+public class DivideArrayToArrays {
+    public int[][] divideArray(int[] nums, int k) { // time complexity - O(n log n)
+        Arrays.sort(nums);
+        int index = 0;
+        int[][] result = new int[nums.length / 3][3];
+        for (int i = 0; i < nums.length; i += 3) {
+            if (nums[i] + k < nums[i + 2])
+                return new int[0][0];
+            result[index][0] = nums[i];
+            result[index][1] = nums[i + 1];
+            result[index++][2] = nums[i + 2];
+        }
+        return result;
+    }
+}
