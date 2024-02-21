e0f110d00c278e7e91eedfdea9da07c60956f451
Subhrajeet Ghosh <subhrajeet.ghosh@oracle.com>
2024-02-21 16:28:31 +0530

    21feb24

---
+++ BitAndMerge.java
@@ -0,0 +1,24 @@
+/**
+ * https://leetcode.com/problems/bitwise-and-of-numbers-range
+ * 
+ * @author subhrajeetghosh
+ */
+public class BitAndMerge {
+
+    //Brute force approch
+    public int rangeBitwiseAnd_2ndMethod(int left, int right) {
+        int result = left;
+        for(int i=left+1; i<right; i++) {
+            result &= i;
+        }
+        return result;
+    }
+
+    //optimized approch doing reverse & will reduce step take an example like 5, 12
+    public int rangeBitwiseAnd(int left, int right) {
+        while(right > left) {
+            right = right & right - 1;
+        }
+        return right & left;
+    }
+}
