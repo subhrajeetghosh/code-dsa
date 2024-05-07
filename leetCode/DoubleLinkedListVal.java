d7e33f4556e863e91bd74b37538149d14f83e45e
Subhrajeet Ghosh <subhrajeet.ghosh@oracle.com>
2024-05-07 21:50:25 +0530

    07may24_01

--- leetCode/DoubleLinkedListVal.java
+++ leetCode/DoubleLinkedListVal.java
@@ -30,6 +30,20 @@ public class DoubleLinkedListVal {
         return result.next;
     }
 
+    // Another Optimized Approch
+    public ListNode doubleIt_2(ListNode head) {
+        if (head.val > 4) {
+            head = new ListNode(0, head);
+        }
+        for (ListNode node = head; node != null; node = node.next) {
+            node.val = (node.val * 2) % 10;
+            if (node.next != null && node.next.val > 4) {
+                node.val++;
+            }
+        }
+        return head;
+    }
+
     // Bruteforce Approch
     public ListNode doubleIt_1(ListNode head) {
         long sum = 0;
