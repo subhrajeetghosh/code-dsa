1f20f5799588af85d66e2f0386920f368ee6e229
Subhrajeet Ghosh <subhrajeet.ghosh@oracle.com>
2024-12-02 21:54:41 +0530

    02dec2024

---
+++ leetCode/CheckWordPrefix.java
@@ -0,0 +1,19 @@
+package leetCode;
+
+/**
+ * https://leetcode.com/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence
+ * 
+ * @author subhrajeetghosh
+ */
+public class CheckWordPrefix {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        int index = 1;
+        for (String str : sentence.split(" ")) {
+            if (str.indexOf(searchWord) == 0) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
+}
