dca695126c2a51f0ab39ebefc7586328e608f525
subhrgho <subhrajeet.ghosh@oracle.com>
2024-12-24 21:45:12 -0700

    24dec2024_01

--- Tree/LargestValEachTreeRow.java
+++ Tree/LargestValEachTreeRow.java
@@ -13,6 +13,7 @@ import commonfile.TreeNode;
  * @author subhtajeetghosh
  */
 public class LargestValEachTreeRow {
+    //BFS
     public List<Integer> largestValues(TreeNode root) { //time complexity - O(n)
         if (root == null) return new ArrayList<>();
         List<Integer> list = new ArrayList<>();
@@ -30,5 +31,23 @@ public class LargestValEachTreeRow {
             list.add(currentMax);
         }
         return list;
+    }
+
+    //DFS
+    List<Integer> list;
+    public List<Integer> largestValues_2ndMethod(TreeNode root) { //time complexity - O(n)
+        list = new ArrayList<>();
+        dfs(root, 0);
+        return list;
+    }
+    private void dfs(TreeNode root, int depth) {
+        if(root == null) return;
+        if(list.size() == depth) {
+            list.add(root.val);
+        } else {
+            list.set(depth, Math.max(list.get(depth), root.val));
+        }
+        dfs(root.left, depth+1);
+        dfs(root.right, depth+1);
     }
 }
