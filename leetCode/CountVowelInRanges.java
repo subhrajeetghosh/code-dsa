d7a4f46bebee3e9b284d9d1dd51a4a7151817bca
Subhrajeet Ghosh <subhrajeet.ankhi@gmail.com>
2025-01-02 19:39:52 -0700

    02jan2025_1

--- leetCode/CountVowelInRanges.java
+++ leetCode/CountVowelInRanges.java
@@ -2,7 +2,7 @@ package leetCode;
 
 public class CountVowelInRanges {
     // Bruteforce
-    public int[] vowelStrings(String[] words, int[][] queries) {
+    public int[] vowelStrings_2ndMethod(String[] words, int[][] queries) { // time complexity - O(k * n)
         boolean[] isVowel = new boolean[words.length];
         for (int i = 0; i < words.length; i++) {
             if (isVowel(words[i].charAt(0)) && isVowel(words[i].charAt(words[i].length() - 1))) {
@@ -24,5 +24,22 @@ public class CountVowelInRanges {
 
     private boolean isVowel(Character c) {
         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+
+    // Optimized
+    public int[] vowelStrings(String[] words, int[][] queries) { //time complexity - O(n + k)
+        int[] prefixSum = new int[words.length];
+        int totalCount = 0;
+        for (int i = 0; i < words.length; i++) {
+            if (isVowel(words[i].charAt(0)) && isVowel(words[i].charAt(words[i].length() - 1))) {
+                totalCount++;
+            }
+            prefixSum[i] = totalCount;
+        }
+        int[] result = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            result[i] = prefixSum[queries[i][1]] - (queries[i][0] == 0 ? 0 : prefixSum[queries[i][0] - 1]);
+        }
+        return result;
     }
 }
