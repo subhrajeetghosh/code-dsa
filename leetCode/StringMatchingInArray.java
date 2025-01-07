888899b078608cf798126e1abbc526583a90a44b
Subhrajeet Ghosh <subhrajeet.ankhi@gmail.com>
2025-01-07 11:04:01 -0700

    07jan2025_1

--- leetCode/StringMatchingInArray.java
+++ leetCode/StringMatchingInArray.java
@@ -2,7 +2,9 @@ package leetCode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * https://leetcode.com/problems/string-matching-in-an-array
@@ -22,18 +24,60 @@ public class StringMatchingInArray {
         }
         return result;
     }
-    //Another Approch
-    public List<String> stringMatching_2ndMethod(String[] words) { //time complexity - O(n log n + n^2 m)
+
+    // Another Approch
+    public List<String> stringMatching_2ndMethod(String[] words) { // time complexity - O(n log n + n^2 m)
         List<String> result = new ArrayList<String>();
         Arrays.sort(words, (a, b) -> Integer.compare(a.length(), b.length()));
-        for(int i=0; i<words.length; i++) {
-            for(int j=i+1; j<words.length; j++) {
-                if(words[j].contains(words[i])) {
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                if (words[j].contains(words[i])) {
                     result.add(words[i]);
                     break;
                 }
             }
         }
         return result;
+    }
+
+    class Trie {
+        int freq;
+        Map<Character, Trie> children;
+
+        public Trie() {
+            freq = 0;
+            children = new HashMap<Character, Trie>();
+        }
+    }
+
+    public List<String> stringMatching_3rdMethod(String[] words) { // time complexity - O(n m^2)
+        List<String> result = new ArrayList<>();
+        Trie root = new Trie();
+        for (String word : words) {
+            for (int i = 0; i < word.length(); i++) {
+                insertTrie(root, word.substring(i));
+            }
+        }
+        for (String string : words) {
+            if (isFound(string, root)) {
+                result.add(string);
+            }
+        }
+        return result;
+    }
+
+    private void insertTrie(Trie node, String word) {
+        for (Character c : word.toCharArray()) {
+            node.children.putIfAbsent(c, new Trie());
+            node = node.children.get(c);
+            node.freq++;
+        }
+    }
+
+    private boolean isFound(String str, Trie node) {
+        for (Character c : str.toCharArray()) {
+            node = node.children.get(c);
+        }
+        return node.freq > 1;
     }
 }
