90b4967c2fa6b1ef5fd2b6976a667fbdfd6d4f9f
Subhrajeet Ghosh <subhrajeet.ankhi@gmail.com>
2025-03-03 20:26:19 -0700

    3march2025

---
+++ leetCode/NumberIsSumPowerThree.java
@@ -0,0 +1,41 @@
+package leetCode;
+
+/**
+ * https://leetcode.com/problems/check-if-number-is-a-sum-of-powers-of-three/
+ */
+public class NumberIsSumPowerThree {
+    public boolean checkPowersOfThree(int n) { // time complexity - O(2^ log 3(n))
+        int count = 0;
+        while (Math.pow(3, count) < n) {
+            count++;
+        }
+        return isPowerOfThree(n, 0, count, 0);
+    }
+
+    private boolean isPowerOfThree(int n, int index, int limit, int currentSum) {
+        if (currentSum == n) {
+            return true;
+        } else if (currentSum > n) {
+            return false;
+        }
+        for (int i = index; i <= limit; i++) {
+            currentSum += Math.pow(3, i);
+            if (isPowerOfThree(n, i + 1, limit, currentSum)) {
+                return true;
+            }
+            currentSum -= Math.pow(3, i);
+        }
+        return false;
+    }
+
+    // Optimized solution
+    public boolean checkPowersOfThree_2ndMethod(int n) { // time complexity - O(log 3(n))
+        while (n > 0) {
+            if (n % 3 == 2) {
+                return false;
+            }
+            n /= 3;
+        }
+        return true;
+    }
+}
