05f235a21eff75bed8b0b8d496111b289516266b
Subhrajeet Ghosh <subhrajeet.ankhi@gmail.com>
2025-03-23 23:29:59 -0700

    23march2025_02

--- leetCode/CountDaysWithoutMeetings.java
+++ leetCode/CountDaysWithoutMeetings.java
@@ -1,6 +1,7 @@
 package leetCode;
 
 import java.util.Arrays;
+import java.util.TreeMap;
 
 /**
  * https://leetcode.com/problems/count-days-without-meetings
@@ -8,7 +9,7 @@ import java.util.Arrays;
  * @author subhrajeetghosh
  */
 public class CountDaysWithoutMeetings {
-    public int countDays(int days, int[][] meetings) { //time complexity - O(nlogn)
+    public int countDays(int days, int[][] meetings) { // time complexity - O(nlogn)
         Arrays.sort(meetings, (a, b) -> a[0] - b[0]);
         int currentStart = 1, result = 0;
         for (int[] meeting : meetings) {
@@ -20,6 +21,42 @@ public class CountDaysWithoutMeetings {
         if (currentStart <= days) {
             result += days - currentStart + 1;
         }
+        return result;
+    }
+
+    public int countDays_2ndMethod(int days, int[][] meetings) { // time complexity - O(n+m)
+        int[] arr = new int[days + 2];
+        for (int[] meeting : meetings) {
+            arr[meeting[0]]++;
+            arr[meeting[1] + 1]--;
+        }
+        int result = 0;
+        for (int i = 1; i <= days; i++) {
+            arr[i] += arr[i - 1];
+            if (arr[i] == 0) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public int countDays_3rdMethod(int days, int[][] meetings) { //time complexity - O(nlogn)
+        TreeMap<Integer, Integer> dayMap = new TreeMap<>();
+        int previousDay = days, prefixSum = 0, result = 0;
+        for(int[] meeting : meetings) {
+            previousDay = Math.min(previousDay, meeting[0]);
+            dayMap.put(meeting[0], dayMap.getOrDefault(meeting[0], 0) + 1);
+            dayMap.put(meeting[1] + 1, dayMap.getOrDefault(meeting[1] + 1, 0) - 1);
+        }
+        result += previousDay - 1;
+        for(int key : dayMap.keySet()) {
+            if(prefixSum == 0) {
+                result += key - previousDay;
+            }
+            prefixSum += dayMap.get(key);
+            previousDay = key;
+        }
+        result += days - previousDay + 1;
         return result;
     }
 }
