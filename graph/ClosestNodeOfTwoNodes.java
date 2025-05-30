d6d44a071299a81e31f540dd0b709bd046a712bc
Subhrajeet Ghosh <subhrajeet.ankhi@gmail.com>
2025-05-30 13:28:28 -0700

    30may2025

---
+++ graph/ClosestNodeOfTwoNodes.java
@@ -0,0 +1,42 @@
+package graph;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * https://leetcode.com/problems/find-closest-node-to-given-two-nodes
+ * 
+ * @author subhrajeetghosh
+ */
+public class ClosestNodeOfTwoNodes {
+    public int closestMeetingNode(int[] edges, int node1, int node2) { // time complexity - O(n)
+        int[] node1Dist = new int[edges.length];
+        int[] node2Dist = new int[edges.length];
+        Set<Integer> visited = new HashSet<>();
+        Arrays.fill(node1Dist, -1);
+        Arrays.fill(node2Dist, -1);
+        dfs(edges, node1, 0, node1Dist, visited);
+        visited.clear();
+        dfs(edges, node2, 0, node2Dist, visited);
+        int result = -1, minDist = Integer.MAX_VALUE;
+        for (int i = 0; i < edges.length; i++) {
+            if (node1Dist[i] != -1 && node2Dist[i] != -1) {
+                int currentMinDist = Math.max(node1Dist[i], node2Dist[i]);
+                if (currentMinDist < minDist) {
+                    minDist = currentMinDist;
+                    result = i;
+                }
+            }
+        }
+        return result;
+    }
+
+    private void dfs(int[] edges, int currentNode, int currentDist, int[] distArr, Set<Integer> visited) {
+        visited.add(currentNode);
+        distArr[currentNode] = currentDist;
+        if (edges[currentNode] != -1 && !visited.contains(edges[currentNode])) {
+            dfs(edges, edges[currentNode], currentDist + 1, distArr, visited);
+        }
+    }
+}
